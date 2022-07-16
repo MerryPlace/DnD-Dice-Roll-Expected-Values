0de4f2d17cb92f8b48ea23924c8fdcea905a736d
Noah Ortega <38738663+NoahOrtega@users.noreply.github.com>
2022-07-16 00:33:14 -0400

    Fixed advantage/disadvantage rolls
    This man helped me :) 
    https://www.youtube.com/watch?v=X_DdGRjtwAo&ab_channel=Stand-upMaths

--- main.py
+++ main.py
@@ -1,43 +1,34 @@
 import re
 
 def norm_roll(num, sides):
-  print("Expected Value of " + str(num) +"d"+ str(int(sides)) +" is: " + str(num * (sides/2.0 + .5)) + "\n")
+  print("Expected Value of a " + str(num) +"d"+ str(int(sides)) +" is: \n" + str(num * (sides/2.0 + .5)) + "\n")
 
 def dis_roll(num, sides):
-  denom = sides**2
-
-  numer = (sides*2) - 1
-
-  total = 0.0
-  for side in range(1,sides + 1, 1): #from 1 to max side
-    total += (side*(numer))/denom
-    numer -= 2
-
-  print("Expected Value of " + str(num) +"d"+ str(int(sides)) +" with disadvantage is: " + str(num*total) + "\n")
+  if(num>1):
+    result = (1/(num+1)) * sides + .5
+  else:  
+    result = norm_roll(num, sides)
+  print("Expected Value of rolling a " +"d"+ str(sides) +" " +str(num)+ " times with disadvantage: \n" + str(result) + " (approximately)\n")
 
 def adv_roll(num, sides):
-  denom = sides**2
-  numer = (sides*2) - 1
-
-  total = 0.0
-  for side in range(sides,0, -1): #from max side to 1
-    total += (side*(numer))/denom
-    numer -= 2
-
-  print("Expected Value of " + str(num) +"d"+ str(int(sides)) +" with advantage is: " + str(num*total) + "\n")
+  if(num>1):
+    result = (num/(num+1)) * sides + .5
+  else:  
+    result = norm_roll(num, sides)
+  print("Expected Value of rolling a " +"d"+ str(sides) +" " +str(num)+ " times with advantage: \n" + str(result) + " (approximately)\n")
 
 def dice_parse(dice):
-  for x in dice:
-    halves = x.split("d")
-    front = halves[0] #first half (becomes number of dice)
-    faces = int(halves[1]) #number of faces on dice
-    first = front[0] #first char, to check for adv/dis
-
+    first = dice[0] #first char, to check for adv/dis
     if(first== '-' or first== '+'):
-      front = front.replace('-', '')
-      front = front.replace('+', '')
-
-    num = int(front) #number of dice
+      dice = dice.replace('-', '')
+      dice = dice.replace('+', '')
+    if(dice[0] == 'd'):
+      num = 1
+      faces = int(dice.replace('d', ''))
+    else:
+      halves = dice.split("d")
+      num = int(halves[0]) #number of dice
+      faces = int(halves[1]) #number of faces on dice
 
     if(num > 0 and faces > 0):
       if(first == '-'): #if disadvantage
@@ -47,27 +38,32 @@ def dice_parse(dice):
       else:
         norm_roll(num, faces)
     else:
-      print("Numbers must be positive. You can't roll negative sided dice!")
-
+      print("You can't roll dice that don't exist! Numbers should be positive!")
 
 
 ##PROGRAM START##
 data = ""
-while(data != 'q' and data != 'Q'):
+while(data != 'q'):
   print("\nEnter the code of your desired selection:")
-  data = input()
-
-  if(data == "h"):
-    print("HELP:\nFormat dice as 'ndx' where n is the number of dice and x is the number of sides. \n-Ex: \"2d6\" would be the mean value of 2 dice that are 6 sided" 
+  data = input().lower().strip()
+  data = re.sub(' +', ' ', data)
+  if(data == "h" or data == "H"):
+    print("HELP:\nFormat dice input as 'xdn' where x is the number of dice and n is the number of sides. \n-Ex: \"2d6\" would be the value of 2 dice that are 6 sided" 
     + "\nTo roll with advantage put a '+' before the dice code. \n-Ex: \"+1d8\" would roll 2 d8 and take the higher value."
     + "\nTo roll with disadvantage put a '-' before the dice code. \n-Ex: \"-1d8\" would roll 2 d8 and take the lower value."
     + "\nYou can see the average of multiple types of rolls in one input by seperating several 'ndx' inputs with any other character such as a space. \n-Ex: \"4d6 6d4 -3d12\" would roll 4 d6's, 6 d4's, and 3 d12's with disadvantage."
     + "\n\nENTER 'q' IF YOU WOULD LIKE TO QUIT")
   else:
-    dice = re.findall("[-+]?[0-9]+[dD][0-9]+", data)
-    if(len(dice) == 0 and data != 'q'):
-      print("Input formatted incorrectly. Please enter 'h' for help or 'q' to quit")
-    else:
-      dice_parse(dice)
+    print("\n")
+    entries = data.split(" ")
+    for entry in entries:
+      match = re.fullmatch("^([-+]?(?:[1-9][0-9]*)*[d][0-9]+)$", entry)
+
+      print(entry +": ")
+      if(match != None):
+         dice_parse(entry)
+      else:
+        print("Input formatted incorrectly. Please enter 'h' for help or 'q' to quit\n")
+
 
 print("\nThank you for testing your luck!")
